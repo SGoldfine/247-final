7db322cb47294d8fbc5b2ac265c645d0edfc817f
Spencer Goldfine <goldfis@email.sc.edu>
2023-11-28 14:26:40 -0500

    added javafx stuff

---
+++ demo/src/main/java/demo/PrimaryController.java
@@ -0,0 +1,17 @@
+package demo;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+
+    @FXML
+    private void switchToHello() throws IOException {
+        App.setRoot("hello");
+    }
+}
