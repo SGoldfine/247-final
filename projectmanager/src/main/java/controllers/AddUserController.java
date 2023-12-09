d4355c2f9e90583a7d39f882593176bd2b0a8349
Aidan McClelland <a-meister88@outlook.com>
2023-12-08 22:31:20 -0500

    worked on add user

--- projectmanager/src/main/java/controllers/AddUserController.java
+++ projectmanager/src/main/java/controllers/AddUserController.java
@@ -1,65 +1,45 @@
+package controllers;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.text.Text;
-import javafx.scene.text.Font;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
-import java.nio.charset.StandardCharsets;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import javafx.application.Platform;
-import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
-import projectmanager.App;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class AddUserController {
-
-    @FXML
-    private Text addUserText;
 
     @FXML
     private TextField findUserTextField;
 
     @FXML
-    private ComboBox<String> assignRoleComboBox;
-
-    @FXML
     private TextArea searchResultsTextArea;
 
-    @FXML
-    private Slider slider;
-
     // Sample list of users
-    private JSONArray userJSONArray;
+    private List<String> userJsonList;
 
     @FXML
     private void initialize() {
-        // Initialize the ComboBox with sample role options
-        assignRoleComboBox.getItems().addAll("Developer", "Administrator", "User");
-
         // Load the user list from the JSON file
         loadUserList();
     }
 
     private void loadUserList() {
         try {
-            InputStream inputStream = getClass().getResourceAsStream("/path/to/user.json");
+            InputStream inputStream = getClass().getResourceAsStream("/data/user.json");
 
             if (inputStream != null) {
-                String jsonString = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
-
-                // Parse the JSON array manually
-                userJSONArray = new JSONArray(jsonString);
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                    // Read the JSON data into a List of Strings
+                    userJsonList = reader.lines().collect(Collectors.toList());
+                }
             }
 
         } catch (IOException e) {
@@ -78,9 +58,8 @@ public class AddUserController {
         String searchTerm = findUserTextField.getText().toLowerCase();
         StringBuilder results = new StringBuilder();
 
-        for (int i = 0; i < userJSONArray.length(); i++) {
-            JSONObject jsonUser = userJSONArray.getJSONObject(i);
-            String username = jsonUser.getString("username");
+        for (String jsonUser : userJsonList) {
+            String username = extractUsername(jsonUser);
 
             // Filter usernames based on the search term
             if (username.toLowerCase().contains(searchTerm)) {
@@ -89,5 +68,12 @@ public class AddUserController {
         }
 
         searchResultsTextArea.setText(results.toString());
+    }
+
+    private String extractUsername(String jsonUser) {
+        // Extract the username from the JSON string (you may need to adjust this based on your actual JSON structure)
+        int usernameIndex = jsonUser.indexOf("\"username\":\"") + 12;
+        int endIndex = jsonUser.indexOf("\"", usernameIndex);
+        return jsonUser.substring(usernameIndex, endIndex);
     }
 }
