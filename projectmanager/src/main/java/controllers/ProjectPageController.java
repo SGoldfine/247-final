602e37eaecb61a37174c6f38d45001a9692b7342
Spencer Goldfine <goldfis@email.sc.edu>
2023-12-08 22:08:19 -0500

    added return and logout, task page needs to be finished

--- projectmanager/src/main/java/controllers/ProjectPageController.java
+++ projectmanager/src/main/java/controllers/ProjectPageController.java
@@ -3,6 +3,8 @@ package controllers;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
@@ -19,30 +21,47 @@ public class ProjectPageController implements Initializable {
     private Project currentProject; // Assuming you have a reference to the current project
 
     @FXML
-    private void onAddTaskClicked(MouseEvent event) {
-        Task newTask = createNewTask(); // Replace with the actual method to create a new task
-        //currentProject.addTask(newTask); // Assuming the Project class has an addTask method
-        tasksListView.getItems().add(newTask);
+    private void onAddTaskClicked(MouseEvent event) throws IOException{
+        // Task newTask = createNewTask(); // Replace with the actual method to create a new task
+        // //currentProject.addTask(newTask); // Assuming the Project class has an addTask method
+        // tasksListView.getItems().add(newTask);
+        App.setRoot("add_task");
     }
 
     @FXML 
-    private void onAddUserClicked(MouseEvent event) {
-        
+    private void onAddUserClicked(MouseEvent event) throws IOException {
+        App.setRoot("add_user");
     }
 
-    private Task createNewTask() {
-        // Logic to create a new task, replace this with your actual task creation logic
-        return new Task("New Task"); // Replace with the actual constructor
+    @FXML 
+    private void onTaskClicked(MouseEvent event) throws IOException {
+        App.setRoot("task_page");
     }
+    
+    @FXML 
+    private void onReturnClicked(MouseEvent event) throws IOException {
+        App.setRoot("project_manager");
+    }
+
+    @FXML 
+    private void onLogoutClicked(MouseEvent event) throws IOException {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    // private Task createNewTask() {
+    //     // Logic to create a new task, replace this with your actual task creation logic
+    //     return new Task("New Task"); // Replace with the actual constructor
+    // }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Initialize the controller, like setting the current project
-        currentProject = getCurrentProject(); // Replace with the actual method to get the current project
+        // // Initialize the controller, like setting the current project
+        // currentProject = getCurrentProject(); // Replace with the actual method to get the current project
     }
 
-    private Project getCurrentProject() {
-        // Logic to get the current project, replace this with your actual logic
-        return new Project("Example Project"); // Replace with the actual constructor
-    }
+    // private Project getCurrentProject() {
+    //     // Logic to get the current project, replace this with your actual logic
+    //     return new Project("Example Project"); // Replace with the actual constructor
+    // }
 }
