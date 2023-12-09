d1b592361bf56218a734f899f48178b163349262
Spencer Goldfine <goldfis@email.sc.edu>
2023-12-08 22:41:33 -0500

    added return and logout to new page, styled new page. removed methods

--- projectmanager/src/main/java/controllers/AddUserController.java
+++ projectmanager/src/main/java/controllers/AddUserController.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import projectmanager.App;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,58 +25,19 @@ public class AddUserController {
     @FXML
     private TextArea searchResultsTextArea;
 
-    // Sample list of users
-    private List<String> userJsonList;
+     @FXML 
+    private void onReturnClicked(MouseEvent event) throws IOException {
+        App.setRoot("project_manager");
+    }
+
+    @FXML 
+    private void onLogoutClicked(MouseEvent event) throws IOException {
+        Platform.exit();
+        System.exit(0);
+    }
 
     @FXML
     private void initialize() {
-        // Load the user list from the JSON file
-        loadUserList();
-    }
-
-    private void loadUserList() {
-        try {
-            InputStream inputStream = getClass().getResourceAsStream("/data/user.json");
-
-            if (inputStream != null) {
-                try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
-                    // Read the JSON data into a List of Strings
-                    userJsonList = reader.lines().collect(Collectors.toList());
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            // Handle the exception (e.g., show an alert to the user)
-        }
-    }
-
-    @FXML
-    private void onFindUserTextChanged() {
-        // Handle the user search logic when the text in the search bar changes
-        updateSearchResults();
-    }
-
-    private void updateSearchResults() {
-        String searchTerm = findUserTextField.getText().toLowerCase();
-        StringBuilder results = new StringBuilder();
-
-        for (String jsonUser : userJsonList) {
-            String username = extractUsername(jsonUser);
-
-            // Filter usernames based on the search term
-            if (username.toLowerCase().contains(searchTerm)) {
-                results.append(username).append("\n");
-            }
-        }
-
-        searchResultsTextArea.setText(results.toString());
-    }
-
-    private String extractUsername(String jsonUser) {
-        // Extract the username from the JSON string (you may need to adjust this based on your actual JSON structure)
-        int usernameIndex = jsonUser.indexOf("\"username\":\"") + 12;
-        int endIndex = jsonUser.indexOf("\"", usernameIndex);
-        return jsonUser.substring(usernameIndex, endIndex);
+        
     }
 }
--- projectmanager/src/main/java/controllers/TaskPageController.java
+++ projectmanager/src/main/java/controllers/TaskPageController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -27,56 +28,21 @@ public class TaskPageController implements Initializable {
     @FXML
     private TextField txtNewComment;
 
-    private Task currentTask; // Assuming Task is a class that represents a task
+    @FXML 
+    private void onReturnClicked(MouseEvent event) throws IOException {
+        App.setRoot("project_manager");
+    }
+
+    @FXML 
+    private void onLogoutClicked(MouseEvent event) throws IOException {
+        Platform.exit();
+        System.exit(0);
+    }
 
     // Initialize the controller
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // Initialize the UI elements or load task data
-        loadTaskData(); // Implement this method to load task data into the UI
+       
     }
-
-    // Method to load task data into UI elements
-    private void loadTaskData() {
-        // Fetch task details and populate UI elements
-        // For instance:
-        currentTask = fetchCurrentTask(); // Implement this method to fetch the task
-
-        if (currentTask != null) {
-            txtHoursWorked.setText(String.valueOf(currentTask.getHoursWorked()));
-            txtTaskPriority.setText(currentTask.getPriority());
-            txtTaskStatus.setText(currentTask.getStatus());
-            displayComments(); // Implement this method to show existing comments
-        }
-    }
-
-    // Method to display existing comments
-    private void displayComments() {
-        // Fetch comments associated with the task and display in the text area
-        // For instance:
-        if (currentTask != null) {
-            txtAreaComments.setText(currentTask.getComments());
-        }
-    }
-
-    // Method to handle adding a new comment
-    @FXML
-    private void onAddCommentClicked(MouseEvent event) {
-        String newComment = txtNewComment.getText();
-        
-        // Add the new comment to the task
-        if (currentTask != null && !newComment.isEmpty()) {
-            currentTask.addComment(newComment); // Implement addComment method in Task class
-            displayComments(); // Update the displayed comments
-            txtNewComment.clear(); // Clear the input field after adding the comment
-        } else {
-            lblError.setText("Error: Unable to add an empty comment.");
-        }
-    }
-
-    // Method to navigate back (if needed)
-    @FXML
-    private void back(MouseEvent event) throws IOException {
-        // Implement navigation back to the previous page
-        App.setRoot("previous_page");
+}
     
